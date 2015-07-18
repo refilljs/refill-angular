7744a0e925b101d4b0db232b022a09fbb83f4adc
bolczyslaw <bolczyslaw@gmail.com>
2015-07-18 18:05:54 +0200

    Updated karma to 0.13, refactored test task

--- src/tasks/test.js
+++ src/tasks/test.js
@@ -7,60 +7,72 @@ function getTestTask(options, gulp, mode) {
     var karma = require('karma');
     var testLogger = require('gulp-zkflow-logger')('test');
     var reporters = ['progress'];
+    var server;
 
     if (mode.env !== 'dev') {
       reporters.push('junit');
     }
 
-    karma.server.start({
-      files: options.files,
-      plugins: [
-        require('karma-browserify'),
-        require('karma-jasmine'),
-        require('karma-junit-reporter'),
-        require('karma-phantomjs-launcher')
-      ],
-      logLevel: 'warn',
-      frameworks: ['jasmine', 'browserify'],
-      browserNoActivityTimeout: 120000,
-      singleRun: mode.env !== 'dev',
-      autoWatch: mode.env === 'dev',
-      preprocessors: {
-        'src/**': ['browserify']
+    server = new karma.Server({
+        files: options.files,
+        plugins: [
+          require('karma-browserify'),
+          require('karma-jasmine'),
+          require('karma-junit-reporter'),
+          require('karma-phantomjs-launcher')
+        ],
+        logLevel: 'error',
+        frameworks: ['jasmine', 'browserify'],
+        browserNoActivityTimeout: 120000,
+        singleRun: mode.env !== 'dev',
+        autoWatch: mode.env === 'dev',
+        preprocessors: {
+          'src/**': ['browserify']
+        },
+        browserify: {
+          debug: true,
+          configure: function(bundle) {
+            bundle.on('update', testLogger.start);
+          }
+        },
+        browsers: ['PhantomJS'],
+        junitReporter: {
+          outputDir: options.junitReporterOutputDir
+        },
+        reporters: reporters
       },
-      browserify: {
-        debug: true,
-        transform: [
-          require('browserify-ngannotate')
-        ]
-      },
-      browsers: ['PhantomJS'],
-      junitReporter: {
-        outputFile: options.junitReporterOutputFile
-      },
-      reporters: reporters
-    }, function(exitStatus) {
+      function(exitStatus) {
 
-      var errorMessage = 'task failed';
+        var errorMessage = 'task failed';
 
-      if (mode.env !== 'dev') {
-        if (exitStatus === 0) {
-          testLogger.finished();
-          done();
-          return;
+        if (mode.env !== 'dev') {
+          if (exitStatus === 0) {
+            testLogger.finished();
+            done();
+            return;
+          }
+          testLogger.error({
+            message: errorMessage
+          });
+          done(errorMessage);
         }
-        testLogger.error({
-          message: errorMessage
-        });
-        done(errorMessage);
-      }
 
-    });
+      }
+    );
+
+
 
     if (mode.env === 'dev') {
-      testLogger.finished();
+
+      server.on('run_complete', function() {
+        testLogger.finished();
+      });
+
       done();
+
     }
+
+    server.start();
 
   }
 
@@ -72,6 +84,6 @@ module.exports = {
   getTask: getTestTask,
   defaultOptions: {
     files: ['src/unitTests.js'],
-    junitReporterOutputFile: 'test-results.xml'
+    junitReporterOutputDir: ''
   }
 };
