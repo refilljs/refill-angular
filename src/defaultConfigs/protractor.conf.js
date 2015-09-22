182eb1a92b369104439ffb79a962c5f002a9c842
bolczyslaw <bolczyslaw@gmail.com>
2015-09-22 18:38:40 +0200

    increased protractor timeout, less browsers instances

--- src/defaultConfigs/protractor.conf.js
+++ src/defaultConfigs/protractor.conf.js
@@ -4,14 +4,17 @@ exports.config = {
 
   framework: 'cucumber',
 
+  allScriptsTimeout: 60000,
+  getPageTimeout: 60000,
+
   multiCapabilities: [{
     browserName: 'chrome',
     shardTestFiles: true,
-    maxInstances: 3
+    maxInstances: 2
   }, {
     browserName: 'firefox',
     shardTestFiles: true,
-    maxInstances: 3
+    maxInstances: 2
   }],
 
   baseUrl: 'http://localhost:8001',
