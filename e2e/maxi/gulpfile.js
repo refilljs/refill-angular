33e5e56141c9002e226b5c796c391130bfdf4039
Piotr Baranowski <piotr.baranowski@zaklinaczekodu.com>
2016-03-24 15:49:22 +0100

    issue #60 set up chrome as default karma browser, remove custom e2e conf from e2e/maxi

--- e2e/maxi/gulpfile.js
+++ e2e/maxi/gulpfile.js
@@ -2,9 +2,4 @@
 
 var zkflowAngular = require('zkflow-angular');
 
-zkflowAngular.init({
-  e2e: {
-    customConfigFiles: true,
-    watchConfigFile: 'protractor.conf.js'
-  }
-}, undefined, require('gulp'));
+zkflowAngular.init(undefined, undefined, require('gulp'));
