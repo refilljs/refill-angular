c61960d0e44754a7344ba655608054f630ab14d0
Piotr Baranowski <piotr.baranowski@zaklinaczekodu.com>
2016-10-02 14:04:27 +0200

    updated protractor dependencies, removed firefox from e2e tests

--- src/defaultConfigs/protractor.watch.conf.js
+++ src/defaultConfigs/protractor.watch.conf.js
@@ -2,7 +2,4 @@
 
 var protractorConf = require('./protractor.conf').config;
 
-protractorConf.capabilities = protractorConf.multiCapabilities[0];
-delete protractorConf.multiCapabilities;
-
 exports.config = protractorConf;
