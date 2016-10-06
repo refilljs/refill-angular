c9c2519fea1ff9703b5e081d79b1332b3416cbb5
Piotr Baranowski <piotr.baranowski@zaklinaczekodu.com>
2016-10-06 21:35:51 +0200

    renamed to refill-angular in readme, and renamed env vars to refill

--- src/mode.js
+++ src/mode.js
@@ -4,15 +4,15 @@
  * Allows to set different modes of tasks operation.<br>
  * <br>
  * mode.env - you can set or get environment mode. It can be 'dev', 'prod' or 'test'.<br>
- * By default it is set to 'dev'. You can change it be setting environment variable ZKFLOW_ENV or bamboo_ZKFLOW_ENV.<br>
+ * By default it is set to 'dev'. You can change it be setting environment variable REFILL_ENV or bamboo_REFILL_ENV.<br>
  * <br>
  * For example<br>
- * ZKFLOW_ENV=test gulp webserver<br>
+ * REFILL_ENV=test gulp webserver<br>
  * <br>
  * Tasks can operate diffrently in diffrent environment.<br>
  *
  * @type {{env: string}}
- * @alias module:gulp-zkflow-angular.mode
+ * @alias module:refill-angular.mode
  */
 var mode = {
   env: getEnv('ENV', 'dev'),
@@ -22,7 +22,7 @@ var mode = {
 };
 
 function getEnv(name, defaultValue) {
-  var env = process.env['ZKFLOW_' + name] || process.env['bamboo_ZKFLOW_' + name];
+  var env = process.env['REFILL_' + name] || process.env['bamboo_REFILL_' + name];
 
   if (typeof env === 'undefined') {
     return defaultValue;
